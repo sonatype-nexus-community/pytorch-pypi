b920c7a2e96eed192336b2d6eddacbf22aa6030f
Hervé Boutemy <hboutemy@apache.org>
2024-10-08 23:15:43 +0200

    update only recent compute platforms

--- update-pytorch.sh
+++ update-pytorch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for d in . cpu cpu-cxx11-abi cpu_pypi_pkg cu75 cu80 cu90 cu91 cu92 cu100 cu101 cu102 cu110 cu111 cu113 cu115 cu116 cu117 cu117_pypi_cudnn cu118 cu121 cu124 rocm3.10 rocm3.7 rocm3.8 rocm4.0.1 rocm4.1 rocm4.2 rocm4.3.1 rocm4.5.2 rocm5.0 rocm5.1.1 rocm5.2 rocm5.3 rocm5.4.2 rocm5.5 rocm5.6 rocm5.7 rocm6.0 rocm6.1
+# old non-updated compute platforms:
+# cu75 cu80 cu90 cu91 cu92 cu100 cu101 cu102 cu110 cu111 cu113 cu115 cu116 cu117 cu117_pypi_cudnn
+# rocm3.10 rocm3.7 rocm3.8 rocm4.0.1 rocm4.1 rocm4.2 rocm4.3.1 rocm4.5.2 rocm5.0 rocm5.1.1 rocm5.2 rocm5.3 rocm5.4.2 rocm5.5 rocm5.6 rocm5.7
+for d in . cpu cpu-cxx11-abi cpu_pypi_pkg cu118 cu121 cu124 rocm6.0 rocm6.1
 do
   d="whl/$d"
   [ "$d" == "whl/." ] && d="whl"
