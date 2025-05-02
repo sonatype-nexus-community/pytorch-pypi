1e76cf43c34c6bea4b6207b43c15e1439d712d68
Dowon <ks2515@naver.com>
2025-05-02 18:52:10 +0900

    update pytorch index

--- update-pytorch.sh
+++ update-pytorch.sh
@@ -119,7 +119,7 @@ echo "updates found in PyTorch main index: updating also compute platform specif
 # ignore old non-updated ones:
 # - cu75 cu80 cu90 cu91 cu92 cu100 cu101 cu102 cu110 cu111 cu113 cu115 cu116 cu117 cu117_pypi_cudnn
 # - rocm3.10 rocm3.7 rocm3.8 rocm4.0.1 rocm4.1 rocm4.2 rocm4.3.1 rocm4.5.2 rocm5.0 rocm5.1.1 rocm5.2 rocm5.3 rocm5.4.2 rocm5.5 rocm5.6 rocm5.7
-for d in cpu cpu-cxx11-abi cpu_pypi_pkg cu118 cu121 cu124 rocm6.0 rocm6.1
+for d in cpu cpu-cxx11-abi cpu_pypi_pkg cu118 cu121 cu124 cu126 cu128 rocm6.0 rocm6.1 rocm6.2 rocm6.2.4 rocm6.3 xpu
 do
   updateIndex "whl/$d"
 done
